6b42b689aac0f5063acab1a7391ca98af1099e68
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-05-07 06:37:52 +0530

    extracting args parser

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,13 @@ fn main() {
     let args: Vec<String> = env::args().collect(); //future optimization use args_os
                                                    //dbg!(args);
 
-    let query = &args[1];
-    let file_path = &args[2];
+    let (query, file_path) = parse_config(&args);
 
+    fn parse_config(args: &[String]) -> (&str, &str) {
+        let query = &args[1];
+        let file_path = &args[2];
+        (query, file_path)
+    }
     println!("Searching for {}", query);
     println!("In file {}", file_path);
 
