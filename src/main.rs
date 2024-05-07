79d484b49de48f5e14fd39e2854b40842a175b92
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-05-07 10:02:04 +0530

    change: panic to Result
    make sure to import the std::process modules as well!!

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::fs;
+use std::process;
 fn main() {
     // str::env::args returns an iterator
     //iterator produces a series of values which are collected by collect()
@@ -7,7 +8,10 @@ fn main() {
     let args: Vec<String> = env::args().collect(); //future optimization use args_os
                                                    //dbg!(args);
 
-    let config = Config::build(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
@@ -27,12 +31,12 @@ struct Config {
     file_path: String,
 }
 impl Config {
-    fn build(args: &[String]) -> Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
-            panic!("not enough arguments");
+            return Err("not enough arguments");
         }
         let query = args[1].clone();
         let file_path = args[2].clone();
-        Config { query, file_path }
+        Ok(Config { query, file_path })
     }
 }
