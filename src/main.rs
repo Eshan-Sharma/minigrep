fc95a392aea3368a31daf6367d22c96a16ae2e32
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-05-07 05:59:17 +0530

    create variable to hold query params

--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,11 @@ fn main() {
     //iterator produces a series of values which are collected by collect()
     //this line reads any command line argument passed to it and collects the values into a vector
     let args: Vec<String> = env::args().collect(); //future optimization use args_os
-    dbg!(args);
+                                                   //dbg!(args);
+
+    let query = &args[1];
+    let file_path = &args[2];
+
+    println!("Searching for {}", query);
+    println!("In file {}", file_path);
 }
