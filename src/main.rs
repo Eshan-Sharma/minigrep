4e174e1ad7e7ba049b333c370f7c2117753ab707
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-05-07 05:55:24 +0530

    getting args in cmd

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    // str::env::args returns an iterator
+    //iterator produces a series of values which are collected by collect()
+    //this line reads any command line argument passed to it and collects the values into a vector
+    let args: Vec<String> = env::args().collect(); //future optimization use args_os
+    dbg!(args);
 }
