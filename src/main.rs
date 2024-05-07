c6a2ee6c25ed36980e12fc4cf03cd0f3c46888c5
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-05-07 06:19:43 +0530

    problems with all code in fn main

--- src/main.rs
+++ src/main.rs
@@ -16,3 +16,8 @@ fn main() {
     let content = fs::read_to_string(file_path).expect("Should have been able to read the file"); //Takes in a path and opens that file, returns a std::io::Result<String> of the file content
     println!("With text:\n{content}");
 }
+
+// Problems with the above code
+// 1. Long main function with a lot of variables, the more the variables, harder to keep track of the purpose of each
+// 2. Long function is difficult to reason with, harder to test and harder to change
+// 3. Uses 'expect' to print error message but does not give us the real reason why reading failed
