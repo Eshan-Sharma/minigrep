bfd29217e54ab29c881bede5e757b946cc2b8ba1
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-05-07 17:30:33 +0530

    optimizations using iterators

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ fn main() {
     let args: Vec<String> = env::args().collect(); //future optimization use args_os
                                                    //dbg!(args);
 
-    let config = Config::build(&args).unwrap_or_else(|err| {
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
