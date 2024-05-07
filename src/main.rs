a870d771f68009d643dbb30b669c36012f2b232a
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-05-07 06:45:59 +0530

    grouping config in struct

--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,13 @@ fn main() {
     let args: Vec<String> = env::args().collect(); //future optimization use args_os
                                                    //dbg!(args);
 
-    let (query, file_path) = parse_config(&args);
+    let config = parse_config(&args);
 
-    fn parse_config(args: &[String]) -> (&str, &str) {
-        let query = &args[1];
-        let file_path = &args[2];
-        (query, file_path)
-    }
-    println!("Searching for {}", query);
-    println!("In file {}", file_path);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
 
-    let content = fs::read_to_string(file_path).expect("Should have been able to read the file"); //Takes in a path and opens that file, returns a std::io::Result<String> of the file content
+    let content =
+        fs::read_to_string(config.file_path).expect("Should have been able to read the file"); //Takes in a path and opens that file, returns a std::io::Result<String> of the file content
     println!("With text:\n{content}");
 }
 
@@ -25,3 +21,13 @@ fn main() {
 // 1. Long main function with a lot of variables, the more the variables, harder to keep track of the purpose of each
 // 2. Long function is difficult to reason with, harder to test and harder to change
 // 3. Uses 'expect' to print error message but does not give us the real reason why reading failed
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+    Config { query, file_path }
+}
