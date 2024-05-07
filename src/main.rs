b58d798d31628b1d23145a3a20041ac59a5a1e79
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-05-07 06:49:55 +0530

    creating constructor for config

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ fn main() {
     let args: Vec<String> = env::args().collect(); //future optimization use args_os
                                                    //dbg!(args);
 
-    let config = parse_config(&args);
+    let config = Config::new(&args);
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
@@ -26,8 +26,10 @@ struct Config {
     query: String,
     file_path: String,
 }
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let file_path = args[2].clone();
-    Config { query, file_path }
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+        Config { query, file_path }
+    }
 }
